63e3496b81acc2b2dac3306baad780fe98dbcfad
TonyCWalker <walkertony509@gmail.com>
2019-08-12 19:51:53 -0700

    Closures, Callbacks and Array Metods.

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const myName = 'Tony';
+function returnName () {
+  return console.log(myName);
+}
+returnName();
+// console.log(returnName());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
